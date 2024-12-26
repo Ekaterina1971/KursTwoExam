6e794958d06edfbe42e6f6efb83bc11de5dc2f66
Ekaterina1971 <ivanova71dom@yandex.ru>
2024-12-26 13:05:24 +0300

    действия 1

---
+++ src/main/java/org/skypro/examiner/generator/RandomIndex.java
@@ -0,0 +1,12 @@
+package org.skypro.examiner.generator;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RandomIndex {
+    public int getRandomGenerator(int maxValue) {
+        java.util.Random random = new java.util.Random();
+        int minValue = 0;
+        return random.nextInt(maxValue);
+    }
+}
